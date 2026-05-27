f1d9437d1b8e2cd6d1b3fab799656ac6208886dc
Master Na Chas <master@master-na-chas.local>
2026-05-27 14:57:24 -0400

    fix: remove adapter-libsql from seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 import "dotenv/config";
 
-const prisma = new PrismaClient({
-  adapter: new PrismaLibSql({ url: process.env.DATABASE_URL! }),
-});
+const prisma = new PrismaClient();
 
 async function main() {
   const masters = [
